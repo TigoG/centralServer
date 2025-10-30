bbff29902c196227d5a080ba95f3b6c0924c821a
Henri van der Stelt <hj.vdstelt@gmail.com>
2025-10-30 14:47:42 +0100

    WeatherStation Table

---
+++ Frontend/src/components/Table.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Table() {
+  return (
+    <div>
+      <h2>Data Table Component</h2>
+      {/* Table implementation goes here */}
+    </div>
+  );
+}
+
+export default Table;
