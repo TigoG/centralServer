81cc99ea18506a299ba5c00c8ae2de084ffd0f27
Thomas van Vliet <git@thomasvanvliet.net>
2025-10-30 14:27:54 +0100

    Initialize backend nodejs
    run met `npm test`

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const PORT = 8445;
+
+dotenv.config();
+
+
+app.use(express.json())
+ 
+app.listen (
+    PORT,
+    () => console.log(`It's alive on http://localhost:${PORT}`)
+)
+
+
+app.get('/ping' ,(req,res) => {
+    return res.status(200).send("Listining");
+})
+
