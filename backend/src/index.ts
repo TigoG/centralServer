805ebb56c131f0488b26f73250c03f7d808e7d8e
sebastian <sebas-1059753@1057390-ubuntu-2026-feb.datalabrotterdam.nl>
2025-11-07 11:30:30 +0000

    added new origins

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,10 +8,25 @@ const PORT = 8445;
 
 dotenv.config();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://145.24.237.211:8002',
+  'http://145.24.237.211:80'
+];
+
 app.use(cors({
-  origin: ['*'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
+  origin: function (origin, callback) {
+    // allow requests with no origin (like Postman)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
 app.use(express.json())
