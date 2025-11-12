40caa2ba2d4d521c2ec7c541e18ee7eb5c7859b7
Henri van der Stelt <hj.vdstelt@gmail.com>
2025-11-12 22:09:03 +0100

    nog meer afbeeldingen

--- Frontend/src/About.jsx
+++ Frontend/src/About.jsx
@@ -5,10 +5,11 @@ import image2 from "./assets/test_design-2.png";
 import image3 from "./assets/weerstation1.png";
 import image4 from "./assets/weerstation2.jpg";
 import image5 from "./assets/weerstation3.jpg";
+import image6 from "./assets/weerstation4.jpg";
 function About() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const images = [image1, image2, image3, image4, image5];
+  const images = [image1, image2, image3, image4, image5, image6];
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) =>
